173b99f8daba2cfa78170b7cc1b14c747508ac33
jesusaguilera <jesusaguilera09@gmail.com>
2021-03-31 22:35:25 +0200

    basic_scene

--- src/index.js
+++ src/index.js
@@ -27,7 +27,16 @@ const scene = new THREE.Scene();
  *
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.001, 1000);
+camera.position.z = 3;
 scene.add(camera);
+
+/**
+  * Cube
+  */
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh =  new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 /**
  * Render
