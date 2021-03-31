b008ec8f1fd4d7fdb12ab3fb61780b2814cec32c
jesusaguilera <jesusaguilera09@gmail.com>
2021-03-31 23:34:59 +0200

    transforms

--- src/index.js
+++ src/index.js
@@ -34,6 +34,42 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh =  new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y =  -0.6;
+// mesh.position.z = 1;
+mesh.position.set(0.7, -0.6 , 1);
+//mesh.position.normalize(); // Reduce mesh length to 1
+//mesh.position.length(); // Get length of mesh position
+//mesh.position.distanceTo(camera.position); // Get distance between box and camera
+
+// Scale
+// mesh.scale.x = 2;
+// mesh.scale.y = .5;
+// mesh.scale.z = .5;
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+/* 
+ Math.PI * 2 => 360º
+ Math.PI * 1.5 => 270º
+ Math.PI => 180º
+ Math.PI * 0.5 => 90º
+ Math.PI * 0.25 => 45º
+*/
+// mesh.rotation.reorder("YXZ")
+mesh.rotation.x = Math.PI * 0.25;
+mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.z = 0.5;
+// mesh.rotation.set(0.8, 0.5, 0.5);
+   
+// Look At
+camera.lookAt(mesh.position)
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
 //Render
 const renderer = new THREE.WebGLRenderer({
   canvas,
