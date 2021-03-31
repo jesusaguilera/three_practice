4c76a7d87b917888607c1f9e41e6d1d2d77cf50e
jesusaguilera <jesusaguilera09@gmail.com>
2021-03-31 22:37:49 +0200

    basic scene

--- src/index.js
+++ src/index.js
@@ -3,44 +3,38 @@ import * as THREE from "three"
 // Styles
 import "./assets/scss/main.scss"
 
-/**
-  * Variables
-  */
+// Sizes
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 };
+
+// Canvas
 const canvas = document.querySelector(".c-canvas");
 
-/**
- * Scene
- */
+// Scene
 const scene = new THREE.Scene();
 
 /**
  * Camera
  *
- * fov => Field of view: A bigger number of degrees angles is like fisheye lens and smaller like telephoto lens.
- * aspect ratio
- * near
- * far
+ * Field of view: A bigger number of degrees angles is like fisheye lens and smaller like telephoto lens.
+ * Aspect ratio
+ * Near
+ * Far
  *
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.001, 1000);
 camera.position.z = 3;
 scene.add(camera);
 
-/**
-  * Cube
-  */
+// Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh =  new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-/**
- * Render
- */
+//Render
 const renderer = new THREE.WebGLRenderer({
   canvas,
   // alpha: true,
