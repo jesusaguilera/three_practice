9157414b4110f62b1c66e026afb4503eb11a5be1
jesusaguilera <jesusaguilera09@gmail.com>
2021-04-01 08:50:31 +0200

    animations

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import gsap from "gsap";
 
 // Styles
 import "./assets/scss/main.scss"
@@ -41,4 +42,41 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true
 });
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera);
+
+
+// let time = Date.now();
+// Clock
+const clock = new THREE.Clock();
+
+// Gsap
+// Gsap has its own raq() furnction furnction
+// gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+
+// Animation
+const raq = () => {
+
+  /**
+    * Time
+    * The cube will rotate at the same speed regardless the frame rate of computer (it depending on computer, the frame rate can vary 45, 60, 75...)
+    * The common Frame Rate on computers is 60fps
+    */
+  // const currentTime = Date.now();
+  // const deltaTime = currentTime - time; // Delta => difference between time and previous  time
+  // time = currentTime; // update the time for the next raq call
+  const elapsedTime = clock.getElapsedTime(); // Time elapsed by second
+    
+  // Update objetcs
+  // mesh.rotation.y += 0.002 * deltaTime;
+  mesh.rotation.y = elapsedTime * (Math.PI * 2); // 360º by one second
+  mesh.position.x = Math.cos(elapsedTime);
+  mesh.position.y = Math.sin(elapsedTime);
+  // camera.lookAt(mesh.position)
+
+  // Render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(raq);
+
+}
+
+raq();
