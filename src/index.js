67c2df4781956edc2826bbfe7ff78a917a23313f
jesusaguilera <jesusaguilera09@gmail.com>
2021-04-01 18:16:53 +0200

    fullscreen, pixelRatio and resizing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 // Styles
 import "./assets/scss/main.scss"
@@ -8,6 +9,39 @@ const sizes = {
   width: window.innerWidth,
   height: window.innerHeight
 };
+
+window.addEventListener("resize", (event) => {
+
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+
+})
+
+// Fullscreen and Safari support
+window.addEventListener("dblclick", () => {
+  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+  if (!fullscreenElement) {
+    if (canvas.requestFullscreen) {
+      canvas.requestFullscreen();
+    } else if (canvas.webkitRequestFullscreen) {
+      canvas.webkitRequestFullscreen();
+    }
+  } else {
+    if(document.exitFullscreen) {
+      document.exitFullscreen();
+    }else if(document.webkitExitFullscreen) {
+      document.webkitExitFullscreen()
+    }
+  }
+});
 
 // Canvas
 const canvas = document.querySelector(".c-canvas");
@@ -28,6 +62,11 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.001
 camera.position.z = 3;
 scene.add(camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+// controls.enabled = false;
+controls.enableDamping = true;
+
 // Cube
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
@@ -40,5 +79,26 @@ const renderer = new THREE.WebGLRenderer({
   // alpha: true,
   antialias: true
 });
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera);
+renderer.setSize(sizes.width, sizes.height);
+// if pixel ratio of device is bigger than 2 we take 2
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Clock
+const clock = new THREE.Clock();
+
+// Animation
+const raq = () => {
+
+  // const elapsedTime = clock.getElapsedTime(); // Time elapsed by second
+    
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(raq);
+
+}
+
+raq();
