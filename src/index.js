3c10d35f0f444b381da70c9cca46da0f066125c1
jesusaguilera <jesusaguilera09@gmail.com>
2021-04-02 21:16:12 +0200

    building triangle

--- src/index.js
+++ src/index.js
@@ -69,10 +69,50 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 // Cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh =  new THREE.Mesh(geometry, material);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// const mesh =  new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+const count = 1; // numbers of triangles
+
+// Draw Triangle Vertices
+const vertices = new Float32Array(count * 3 * 3); // count = number of triangles * 3 vertices * 3 values of vertices
+
+for( let i = 0; i < count * 3 * 3; i++) {
+  vertices.[i] = ((Math.random() - 0.5) * 2)
+}
+// Draw Plane Vertices
+/*
+ * const vertices = new Float32Array([
+ *   // Bottom triangle
+ * 	-1.0, -1.0,  1.0, // vertex left-bottom
+ * 	 1.0, -1.0,  1.0, // vertex rigth-bottom
+ * 	 1.0,  1.0,  1.0, // vertex right-top
+ * 
+ *   // Top triangle
+ * 	 1.0,  1.0,  1.0, // vertex top-right
+ * 	-1.0,  1.0,  1.0, // vertex left-top
+ * 	-1.0, -1.0,  1.0  // vertex left-bottom
+ *
+ *   Bottom triangle + Top triangle = Plane
+ * ] );
+ */
+
+// itemSize = 3 because there are 3 values per vertex ( -0.4|1.2|1.0 // -0.3|0.5|0.8 // .. )
+const positionAttribute = new THREE.BufferAttribute( vertices, 3 )
+
+// Empty Geometry
+const geometry = new THREE.BufferGeometry();
+geometry.setAttribute( 'position', positionAttribute );
+
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.DoubleSide} );
+// material.wireframe = true;
+const mesh = new THREE.Mesh( geometry, material );
 scene.add(mesh);
+
+
+
 
 //Render
 const renderer = new THREE.WebGLRenderer({
